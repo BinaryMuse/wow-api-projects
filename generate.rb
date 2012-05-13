5eb5b210879078923d2d2c8be133932032b843ef
Eric Boh <cossish@gmail.com>
2012-05-13 23:16:21 +0200

    Added diablo feature

--- generate.rb
+++ generate.rb
@@ -2,23 +2,35 @@
 
 require 'yaml'
 
-data = YAML.load File.read(File.expand_path('./projects.yml'))
-data = data.inject({}) do |memo, project|
-  language = project["language"]
-  memo[language] ||= []
-  memo[language] << project
-  memo
+entries = File.expand_path("./#{ARGV[0]}.yml")
+if File.exists?(entries)
+  data = YAML.load File.read(entries)
+  data = data.inject({}) do |memo, project|
+    language = project["language"]
+    memo[language] ||= []
+    memo[language] << project
+    memo
+  end
+
+  data.sort.each do |language, projects|
+    puts "[b]#{language}[/b]"
+    projects.sort{ |a, b| a['name'].downcase <=> b['name'].downcase }.each do |project|
+      print "[ul]"
+      print "[li][b]#{project['name']}[/b]"
+      print " by #{project['author']}" if project['author']
+      puts
+      puts project['thread'] if project['thread']
+      puts "#{project['project']}[/li][/ul]"
+    end
+    puts
+  end
+else
+  print <<-EOF
+Please provide a command name
+
+Usage:
+  generate wow
+  generate diablo
+EOF
 end
 
-data.sort.each do |language, projects|
-  puts "[b]#{language}[/b]"
-  projects.sort{ |a, b| a['name'].downcase <=> b['name'].downcase }.each do |project|
-    print "[ul]"
-    print "[li][b]#{project['name']}[/b]"
-    print " by #{project['author']}" if project['author']
-    puts
-    puts project['thread'] if project['thread']
-    puts "#{project['project']}[/li][/ul]"
-  end
-  puts
-end
